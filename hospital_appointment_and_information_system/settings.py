a35855dad22c004a51cb7aac3e391b13eb7bb637
MartaHentosh <marta25gentosh@gmail.com>
2026-05-24 23:50:58 +0300

    try new approach with CSRF_TRUSTED_ORIGINS

--- hospital_appointment_and_information_system/settings.py
+++ hospital_appointment_and_information_system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'd&d%!!iph5fs6i8tmm!2=o=p4zi8fuw67@(&vlf3$(olghu#3h'
 DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
-CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
+CSRF_TRUSTED_ORIGINS = ["http://13.48.214.136"]
 
 CSRF_COOKIE_SECURE = False
 SESSION_COOKIE_SECURE = False
